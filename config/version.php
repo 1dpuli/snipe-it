50655804c0435b05741a39d1fa4d34694884c958
paventhan89 <venthan4805@gmail.com>
2020-10-01 21:14:14 +0530

    version changes

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'app_version' => 'v4.9.5',
-  'full_app_version' => 'v4.9.5 - build 4482-g5b68a321a',
+  'full_app_version' => 'v4.9.5 - build 4482',
   'build_version' => '4482',
   'prerelease_version' => '',
   'hash_version' => 'g5b68a321a',
